8c49e791d889c9d248a48838f6f2fe41718c293d
Joshuaosunlakin3433 <joshuaolaoluwa7@gmail.com>
2022-09-28 13:51:06 -0700

    to-json-string

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,21 +1,10 @@
 #!/usr/bin/python3
-"""read_lines
+"""to_json_string
 """
-import os
+import json
 
-
-def read_lines(filename="", nb_lines=0):
-    """Takes in str filename to read, and n lines w/ int nb_lines
+def to_json_string(my_obj):
+    """Return JSON format of my_obj
     """
 
-
-    with open(filename, encoding="utf-8") as readFile:
-        lineNum = 0
-        while True:
-            line = readFile.readline()
-            lineNum += 1
-            print(line, end='')
-            if lineNum >= nb_lines and nb_lines > 0:
-                break 
-            if not line:
-                break
+    return json.dumps(my_obj)
